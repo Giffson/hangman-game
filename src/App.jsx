fb657e17d4fa81b92e8314e0dc8b9acf75027211
Conscious-Mind <davereddragon@gmail.com>
2024-07-21 13:48:20 +0530

    Missing item

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import MainScreen from './components/Screens/MainScreen'
 
 const App = () => {
   return (
-    <div className='App overflow-hidden bg-no-repeat bg-cover flex items-center justify-center h-screen '  style={{background: `URL(${bg})`}}>
-        <Router>
+      <Router>
             <Routes>
                 <Route path='/' index exact element={<MainMenu/>}/>
                 <Route path='/mainscreen' element={<MainScreen/>}/>
             </Routes>
         </Router>
-    </div>
+  
   )
 }
 
